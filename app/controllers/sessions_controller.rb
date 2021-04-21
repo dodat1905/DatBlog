8e9a43d38bea3d35606e3e5ee9341c26b8dabf84
dodat1905 <dodat1905@gmail.com>
2021-04-21 16:54:03 +0700

    Log in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  def index
+    if current_user
+      redirect_to root_path
+      flash[:danger] = 'You are already logged in'
+    end
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      params[:remember_me] == 'true' ? cookies.signed[:user_id] = user.id : session[:user_id] = user.id
+      redirect_to root_path
+      flash[:success] = 'Login user successful'
+    else
+      flash.now[:danger] = 'User registration is unsuccessful. Username or password is wrong'
+      render :index
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    cookies.delete(:user_id)
+    redirect_to root_path
+    flash[:success] = 'Successfully logged out'
+  end
+
+  private
+
+  attr_reader :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
 
-  resources :users, only: %i[index] do
+  resources :users, only: %i[index edit update] do
     collection do
       get :new, path: :sign_up, as: :sign_up
       post :create, path: :sign_up
     end
   end
 
-  resources :sessions, only: %i[index create], path: :sign_in, as: :sign_in
+  resources :sessions, only: %i[index create], path: 'users/sign_in' do
+    collection do
+      delete :destroy, as: 'users/destroy'
+    end
+  end
 end
